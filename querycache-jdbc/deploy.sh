42ece0424382dcfd22edadefe59f09def1bfeeea
Steven Han <steven.han@sk.com>
2015-03-23 16:51:57 +0900

    updated maven upload script for version 0.12.1 release

--- querycache-jdbc/deploy.sh
+++ querycache-jdbc/deploy.sh
@@ -2,7 +2,7 @@
 version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | egrep '^[0-9]*\.[0-9]*\.[0-9]'`
 echo "$version"
 mvn deploy:deploy-file \
-    -DrepositoryId=skp-snapshots \
-    -Durl=http://mvn.skplanet.com/content/repositories/snapshots/ \
+    -DrepositoryId=skp-releases \
+    -Durl=http://mvn.skplanet.com/content/repositories/releases/ \
     -DpomFile=querycache-jdbc.deploy.pom.xml \
     -Dfile=target/querycache-jdbc-${version}.jar
